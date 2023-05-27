3014caec31a945b31ac4a39abea7afd0f63336ba
Alan Altamira <alan>
2023-05-27 09:53:03 -0600

    feat : 27 may C1

--- app/src/main/java/com/example/royal_app_g2/MainActivity.kt
+++ app/src/main/java/com/example/royal_app_g2/MainActivity.kt
@@ -2,12 +2,28 @@ package com.example.royal_app_g2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import com.example.royal_app_g2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /** */
+    private lateinit var binding : ActivityMainBinding
 
     /** On Create */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.e("MiPrimerApp","Hola desde codigo")
+
+        binding.activityText.text = "Hola desde codigo"
+
+        Toast.makeText(this,"Soy Toast",Toast.LENGTH_SHORT).show()
+
     }
 }
